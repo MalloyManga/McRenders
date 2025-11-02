f40f1ecfca85a79c1e9a80a6ada487a574c9089a
Malloy <li3210361050@gmail.com>
2025-11-02 11:35:49 +0800

    fix: update workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,19 +3,23 @@ import tailwindcss from "@tailwindcss/vite"
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  ssr: false,
   css: ['./app/assets/css/main.css'],
-  app: {
-    baseURL: '/ArtGallery/',
-    head: {
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/ArtGallery/villager.ico' }
-      ]
-    }
-  },
+  ssr: false,
   vite: {
+    build: {
+      assetsInlineLimit: 8192,
+    },
     plugins: [
       tailwindcss(),
     ],
   },
+  app: {
+    baseURL: '/McRenders/',
+    head: {
+      title: 'McRenders',
+      link: [
+        { rel: 'icon', href: '/McRenders/villager.ico' }
+      ],
+    }
+  }
 })
